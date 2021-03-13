1d6831a5e6f5c8b4bf0ba1245469ba5959d2aa71
Joshua Hans <66792834+jhans404@users.noreply.github.com>
2021-03-13 15:08:39 -0500

    Update Squirrel.js

--- src/Components/Squirrel.js
+++ src/Components/Squirrel.js
@@ -4,6 +4,10 @@ function Squirrel(props) {
   return (
     <div className="Squirrel">
       <h3>SQUIRREL CARD!</h3>
+      <p>Name: {props.name}</p>
+      <p>Age: {props.age}</p>
+      <p>Color: {props.color === "Gray" ? "Gray" : "Not Gray"}</p>
+      <p>Runner: {props.running ? "🏃‍♂️" : " "}</p>
     </div>
   );
 }
